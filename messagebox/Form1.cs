42425721a42fc0345084a1d88bbcf96129bdb341
juanmazz <jaunmazziad@gmail.com>
2021-11-26 17:10:14 -0300

    Messagebox

---
+++ messagebox/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace messagebox
+{
+
+    public partial class Form1 : Form
+    {
+        usuario usuario = new usuario();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+
+            if (txtusuario.Text == usuario.uusuario && txtcontraseña.Text == usuario.cotraseña)
+            {
+
+                MessageBox.Show("¡¡Bienvenido!!");
+
+            }
+
+            if (txtusuario.Text != usuario.uusuario && txtcontraseña.Text == usuario.cotraseña)
+            {
+
+                MessageBox.Show("El usuario es incorrecto. ");
+
+            }
+
+            if (txtusuario.Text == usuario.uusuario && txtcontraseña.Text != usuario.cotraseña)
+            {
+
+                MessageBox.Show("La contraseña es incorrecta. ");
+
+            }
+
+            if (txtusuario.Text != usuario.uusuario && txtcontraseña.Text != usuario.cotraseña)
+            {
+
+                MessageBox.Show("El usuario y la contraseña son incorrectos.");
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            usuario.uusuario = "juan";
+            usuario.cotraseña= "123";
+        }
+    }
+
+    class usuario
+    {
+
+        public string uusuario;
+        public string cotraseña;
+
+    }
+}
